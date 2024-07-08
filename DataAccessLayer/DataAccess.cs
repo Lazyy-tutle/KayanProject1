41568fd7d09175e037e6e9dac012662390afc9a4
Lazyy-tutle <132304570+Lazyy-tutle@users.noreply.github.com>
2024-07-08 10:02:28 +0300

    Add project files.

---
+++ DataAccessLayer/DataAccess.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DataAccessLayer
+{
+    public class DataAccess
+    {
+        public string ConnectionString { get; private set; }
+
+        public DynamicParameters Parameters { get; set; } = new DynamicParameters();
+
+        public DataAccess() {
+            IConfiguration configuration = new ConfigurationBuilder().Build();
+            ConnectionString = configuration.GetConnectionString("DBConnection") ?? string.Empty;
+        }
+
+        public IEnumerable<T> ReturnList<T>(string ProcedureName) {
+            using (SqlConnection con = new SqlConnection(ConnectionString)) { 
+                con.Open();
+                return con.Query<T>(ProcedureName, param: Parameters, commandType: System.Data.CommandType.StoredProcedure);
+            }
+        }
+    }
+}
